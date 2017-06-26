cf98e8f56077806cb778f370ad42d4a7c8b1e984
Eric Ihli <ericihli@gmail.com>
2017-06-26 09:25:53 -0700

    Add elpy to emacs init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,13 +20,34 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (fiplr))))
+ '(package-selected-packages (quote (autopair auto-complete fiplr)))
+ '(safe-local-variable-values
+   (quote
+    ((eval progn
+	   (require
+	    (quote find-file-in-project))
+	   (seq ffip-prune-patterns
+		(\`
+		 ("*/python-environments/*"
+		  (\,@ ffip-prune-patterns)))))
+     (ffip-find-options . "-not -size +64k -not -iwholename 'db.sqlite'")
+     (fiplr-ignored-globs quote
+			  ((directories
+			    ("python-environments"))))))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(elpy-enable)
+(load "elpy")
+(define-key elpy-mode-map (kbd "M-n") 'elpy-nav-forward-block)
+(define-key elpy-mode-map (kbd "M-p") 'elpy-nav-backward-block)
+
+(autopair-global-mode)
 
 (add-hook 'python-mode-hook
 	  (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))

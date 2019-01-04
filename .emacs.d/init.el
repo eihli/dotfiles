d164f1260ebccfa6085fb5d4e9ea59f8c15479e5
Eric Ihli <ericihli@gmail.com>
2019-01-03 18:34:32 -0800

    Not installed on linux
    Will apply to Linux if/when needed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -250,7 +250,7 @@
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 ;; Fix shell path on Mac
-(when (memq window-system '(mac ns x))
+(when (memq window-system '(mac))
   (exec-path-from-shell-initialize))
 
 ;; (require 'unicode-fonts)

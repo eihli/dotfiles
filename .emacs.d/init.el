9016b7c03ae446ea2c77fffaf5586695ace3b169
Eric Ihli <ericihli@gmail.com>
2019-06-23 11:09:25 -0700

    Add and configure clj-refactor in emacs init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -277,3 +277,14 @@
 (setq-default tab-width 4)
 (setq-default c-basic-offset tab-width)
 (setq-default cperl-indent-level tab-width)
+
+(require 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+

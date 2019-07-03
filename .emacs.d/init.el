ea3653c14b1faf35734ff03c9e1baedda59f959e
Eric Ihli <eihli@Erics-MacBook-Pro.local>
2019-07-03 15:31:41 -0700

    Add some extensions that aren't retrieved from melpa

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,11 +18,11 @@
 ;; Adds all subdirectories under .emacs.d/extensions
 ;; to the load-path.
 (let* ((subdirs (seq-filter
-		(lambda (f)
-		  (and (file-directory-p (concat "~/.emacs.d/extensions/" f))
-		       (not (or (equal f ".")
-				(equal f "..")))))
-		(directory-files "~/.emacs.d/extensions")))
+		 (lambda (f)
+		   (and (file-directory-p (concat "~/.emacs.d/extensions/" f))
+			(not (or (equal f ".")
+				 (equal f "..")))))
+		 (directory-files "~/.emacs.d/extensions")))
        (subdir-paths (mapcar (lambda (f)
 			       (concat "~/.emacs.d/extensions/" f))
 			     subdirs)))

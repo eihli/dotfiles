4a9c6a69141c122cb8b55c4d6dcb34b3696affd2
Eric Ihli <eihli@owoga.com>
2021-04-01 13:01:17 -0500

    Set tablet area in Wacom

--- .stumpwm.d/wacom.lisp
+++ .stumpwm.d/wacom.lisp
@@ -10,9 +10,15 @@
     (multiple-value-bind (match groups) (cl-ppcre:scan-to-strings "id: (\\d+).*STYLUS" shell-output)
       (let* ((id (aref groups 0))
              (rotate (format nil "xsetwacom set ~A rotate ccw" id))
-             (map-to-output (format nil "xsetwacom set ~A MapToOutput 800x900+1920+0" id)))
+             ;; Aspect ratio of 800x900 will mean we need to adjust area of Stylus also.
+             (map-to-output (format nil "xsetwacom set ~A MapToOutput 800x900+1920+0" id))
+             (tablet-width 15200) ;; xsetwacom get `id` Area
+             (tablet-height 9500)
+             (stylus-area (format nil "xsetwacom set ~A Area 0 0 ~A ~A"
+                                  id (round (* tablet-height (/ 8.0 9))) tablet-height)))
         (run-shell-command rotate t)
-        (run-shell-command map-to-output t)))))
+        (run-shell-command map-to-output t)
+        stylus-area))))
 
 (defcommand do-wacom () ()
   (-do-wacom))

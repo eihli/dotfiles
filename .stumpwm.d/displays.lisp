2427caee343f267a243d2a310933c0aa394ad82d
Eric Ihli <eihli@owoga.com>
2022-06-08 22:50:18 -0700

    Add multi-monitor and Nyxt

--- .stumpwm.d/displays.lisp
+++ .stumpwm.d/displays.lisp
@@ -12,10 +12,13 @@
                 " --output HDMI-1-0"
                 " --primary"
                 " --mode 1920x1080"
+                " --pos 0x840"
                 " --same-as eDP-1"
                 " --output eDP-1"
                 " --mode 1920x1080"
                 " --output DP-1-3"
                 " --rotate left"
                 " --left-of HDMI-1-0"
-                " --auto") t))
+                " --auto") t)
+  (run-shell-command
+   "xrandr --output DP-1-3 --pos 0x-840"))

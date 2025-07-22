f708ad7b2a31d12324bd1750b8c87073dfd7392e
Eric Ihli <eihli@owoga.com>
2025-07-21 23:52:21 -0700

    Add semicolon to ignored caps exit chars, for Norman

--- qmk/config.h
+++ qmk/config.h
@@ -19,10 +19,30 @@
 #define HOME_S LSFT_T(KC_S)
 #define HOME_B LGUI_T(KC_B)
 
+// Left-hand layer 2
+#define LSFTAT   MT(MOD_LSFT, KC_AT)
+#define LALTHASH LALT_T(KC_HASH)
+#define LCTLDLR  LCTL_T(KC_DLR)
+
+// Left-hand layer 3
+#define LSFTEND  LSFT_T(KC_END)
+#define LALTPGUP LALT_T(KC_PGUP)
+#define LCTLPGDN LCTL_T(KC_PGDN)
+
 // Right-hand home row mods
 #define HOME_J RCTL_T(KC_J)
 #define HOME_K LALT_T(KC_K)
 #define HOME_L RSFT_T(KC_L)
 #define HOME_N LGUI_T(KC_N)
+
+// Right-hand layer 2
+#define RCTLAMPR RCTL_T(KC_AMPR)
+#define RALTASTR RALT_T(KC_ASTR)
+#define RSFTLPRN RSFT_T(KC_LPRN)
+
+// Right-hand layer 3
+#define RCTLLEFT RCTL_T(KC_LEFT)
+#define RALTDOWN LALT_T(KC_DOWN)
+#define RSFTUP RSFT_T(KC_UP)
 
 #endif // CONFIG_H_

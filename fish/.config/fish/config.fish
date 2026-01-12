c245ecac9a0130f751c08d3cded85e6413a19f1c
Eric Ihli <eihli@owoga.com>
2026-01-11 21:59:45 -0800

    Add $HOME/go/bin to PATH in bash and fish
    Go installs binaries to ~/go/bin by default. Both shells
    now include this conditionally (bash checks dir exists,
    fish_add_path handles it automatically).

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -49,6 +49,7 @@ end
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/bin
 fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/go/bin
 # macOS: Homebrew (Apple Silicon, Intel) + MacPorts
 fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
 fish_add_path /usr/local/bin /usr/local/sbin
